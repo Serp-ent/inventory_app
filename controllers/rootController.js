6c53a67eba44d511b07d96eaf79bca7fb2e2012b
kenny <kenny909@mat.umk.pl>
2024-08-03 00:38:12 +0200

    Add button next to trainer name for adding pokemons

--- controllers/rootController.js
+++ controllers/rootController.js
@@ -1,3 +1,4 @@
+const { idleCount } = require('../db/pool');
 const db = require('../db/queries');
 const asyncHandler = require('express-async-handler');
 
@@ -7,11 +8,14 @@ const listItems = asyncHandler(async (req, res) => {
 
   rows.forEach(row => {
     if (!trainers[row.trainer_name]) {
-      trainers[row.trainer_name] = [];
+      trainers[row.trainer_name] = {
+        id: row.trainer_id,
+        pokemons: [],
+      };
     }
 
     if (row.pokemon_name) {
-      trainers[row.trainer_name].push(row.pokemon_name);
+      trainers[row.trainer_name].pokemons.push(row.pokemon_name);
     }
   });
 
