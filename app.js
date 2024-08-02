d17bfeddc27ab1fdbf797b5102ede1891da9c4e7
kenny <kenny909@mat.umk.pl>
2024-08-02 16:48:15 +0200

    Add simple error handling

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.get('/', (req, res) => {
   res.send('Simple pokemon inventory');
 });
 
+app.use((req, res, next) => {
+  res.status(404).send('Page not found');;
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send(err.message);
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Express server successfully start at port ${PORT}`);
