8d9fa07bea997dc0970e49bb1281681c7d557ad5
kenny <kenny909@mat.umk.pl>
2024-08-03 00:25:27 +0200

    Add validation to adding trainer

--- controllers/trainersController.js
+++ controllers/trainersController.js
@@ -1,5 +1,6 @@
 const db = require('../db/queries');
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 
 const listTrainers = asyncHandler(async (req, res) => {
   const trainers = await db.getAllTrainers();
@@ -14,13 +15,33 @@ const addTrainerGet = asyncHandler(async (req, res) => {
   res.render('addTrainer', { title: "Add new trainer" });
 });
 
-const addTrainerPost = asyncHandler(async (req, res) => {
-  console.log(req.body);
-  const { trainerName, trainerAge } = req.body;
-  await db.addTrainer(trainerName, trainerAge);
+const validateTrainer = [
+  body('trainerName').trim()
+    .notEmpty().withMessage('Trainer name cannot be empty')
+    .isAlpha().withMessage('Trainer name should contain only letters')
+    .isLength({ min: 1, max: 50 }).withMessage('Trainer name length should be between 1 and 50'),
+  body('trainerAge').trim()
+    .notEmpty().withMessage("Trainer age cannot be empty")
+    .isInt({ min: 1, max: 1000 }).withMessage('Trainer age must be number must be between 1 and 1000'),
+];
 
-  res.redirect('/');
-});
+const addTrainerPost = [
+  validateTrainer,
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render('addTrainer', {
+        title: "Add new trainer",
+        errors: errors.array(),
+      });
+    }
+
+    const { trainerName, trainerAge } = req.body;
+    await db.addTrainer(trainerName, trainerAge);
+
+    res.redirect('/');
+  }),
+];
 
 
 module.exports = {
